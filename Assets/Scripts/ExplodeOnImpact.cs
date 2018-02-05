ff9ada79cc64dcf128277efbccd1ebeebeed78f6
GideonUng <gidcheen@gmail.com>
2018-02-05 02:17:34 +0100

    Projectiles dont collide with eachother anymore

--- Assets/Scripts/ExplodeOnImpact.cs
+++ Assets/Scripts/ExplodeOnImpact.cs
@@ -6,7 +6,7 @@ public class ExplodeOnImpact : MonoBehaviour
 {
 	[SerializeField] Object explosion = null;
 
-	private void OnCollisionEnter(Collision collision)
+	protected virtual void OnCollisionEnter(Collision collision)
 	{
 		var explosionInstance = (GameObject)Instantiate(explosion);
 		explosionInstance.transform.position = collision.contacts[0].point;
---
+++ Assets/Scripts/FastExplodeOnImpact.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FastExplodeOnImpact : ExplodeOnImpact
+{
+	Vector3 lastPos;
+
+	private void Start()
+	{
+		lastPos = transform.position;
+	}
+
+	void Update()
+	{
+		lastPos = transform.position;
+	}
+
+	protected override void OnCollisionEnter(Collision collision)
+	{
+		base.OnCollisionEnter(collision);
+	}
+}
